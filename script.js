c10914af693095c9687581761743fb3fb1263ad8
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-12 02:01:14 -0700

    Refactor code to use size instead of both cols and rows

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function createGrid(rows, cols) {
+const DEFAULT_SIZE = 16;
+
+function createGrid(size) {
     let container = document.querySelector('#container');
 
-    for (let i = 0; i < (rows * cols); i++) {
+    for (let i = 0; i < (size * size); i++) {
         let div = document.createElement("div");
         div.classList.add('cell');
 
-        div.style.width = (512 / cols) + 'px';
-        div.style.height = (512/ rows) + 'px';
+        div.style.width = (512 / size) + 'px';
+        div.style.height = (512/ size) + 'px';
 
         // Change cell to black when hovered over
         div.addEventListener('mouseover', () => {
@@ -22,17 +24,16 @@ function clearGrid() {
     container.innerHTML = "";
 }
 
-// Create grid
-let rows = 16;
-let cols = 16;
-createGrid(rows, cols);
+// Create initial grid
+let size = DEFAULT_SIZE;
+createGrid(size);
 
 // Set up event listener for button
 let btn = document.querySelector('button');
 
 
 btn.addEventListener('mousedown', () => {
-    rows = cols = prompt("Enter a new number of rows and cols");
+    size = prompt("Enter a new number of rows and cols");
     clearGrid();
-    createGrid(rows, cols);
+    createGrid(size);
 });

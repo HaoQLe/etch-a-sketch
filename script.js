c19668ed4997ba3269bb60fbb5fc746eae74611c
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-11 22:55:36 -0700

    Finish method to create 16x16 grid

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function createGrid() {
     let container = document.querySelector('#container');
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < (16 * 16); i++) {
         let div = document.createElement("div");
+        div.classList.add('cell');
         container.appendChild(div);
-        div.textContent = i;
     }
 }
 

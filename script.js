4e68bf8648e9761f62b231fec9c774418c130dff
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-11 23:11:10 -0700

    Create new button to set rows and cols

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function createGrid() {
+function createGrid(rows, cols) {
     let container = document.querySelector('#container');
 
     for (let i = 0; i < (16 * 16); i++) {
         let div = document.createElement("div");
         div.classList.add('cell');
+        
 
+        // Change cell to black when hovered over
         div.addEventListener('mouseover', () => {
             div.style.backgroundColor = 'black';
         });
@@ -13,4 +15,15 @@ function createGrid() {
     }
 }
 
-createGrid();
+// Create grid
+let rows = 16;
+let cols = 16;
+createGrid(rows, cols);
+
+// Set up event listener for button
+let btn = document.querySelector('button');
+
+
+btn.addEventListener('mousedown', () => {
+    rows = cols = prompt("Enter a new number of rows and cols");
+});
